3319514f8703ee3eafcd25cea7d215c68b99c340
olga-82 <134201905+olga-82@users.noreply.github.com>
2023-12-02 09:11:56 +0200

    Update Logger

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -1,16 +1,30 @@
+import manager.TestNgListeners;
 import models.User;
+import models.UserLombok;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+@Listeners(TestNgListeners.class)
 public class RegistrationTests extends TestBase {
-//    @BeforeMethod
-//    public void precondition(){
-//        if (app.getUser().isLogged()) {
-//            app.getUser().logout();
-//        }
-//
-//    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public void postCondAfterMethod(Method method) {
+        if (flagNeedLogout) {
+           app.getUser().buttonOk();
+            flagNeedLogout = false;
+            logger.info("flagNeedLogout = " + flagNeedLogout);
+            logger.info("method info: " + method.getName());
+        } else if (flagNeedOpenMainPage) {
+            app.getUser().buttonLogo();
+            flagNeedOpenMainPage = false;
+            logger.info("flagNeedOpenMainPage = " + flagNeedOpenMainPage);
+        }
+
+    }
 
     @Test
     public void registrationPositive() {
@@ -21,9 +35,14 @@ public class RegistrationTests extends TestBase {
                 .withEmail("nefr"+i+"@gmail.com")
                 .withPassword("Rita12300$");
         app.getUser().openRegestrationForm();
-        app.getUser().fillRegistrationForm(user);
+        app.getUser().fillRegistrationFormWith(user);
         app.getUser().submitLogin();
+        logger.info("registrationPositive starts with credentials "
+                + user.getEmail() + " " + user. getPassword());
+        flagNeedLogout = true;
+        logger.info("flagNeedLogout = " + flagNeedLogout);
         Assert.assertTrue(app.getUser().isLoggedSuccess());
+       // app.getUser().buttonOk();
 
 
     }
@@ -36,11 +55,14 @@ public class RegistrationTests extends TestBase {
                 .withEmail("nefr"+i+"@gmail.com")
                 .withPassword("Rita12300фыб");
         app.getUser().openRegestrationForm();
-        app.getUser().fillRegistrationForm(user);
+        app.getUser().fillRegistrationFormWith(user);
         app.getUser().submitLogin();
-        Assert.assertTrue(app.getUser().isLoggedError());
+        flagNeedOpenMainPage=true;
+        logger.info("flagNeedOpenMainPage = " + flagNeedOpenMainPage);
+        Assert.assertTrue(app.getUser().isLoggedErrorWrongPassword());
+       // app.getUser().buttonLogo();
 
-
+      //  Password must contain 1 uppercase letter, 1 lowercase letter, 1 number and one special symbol of [@$#^&amp;*!]
     }
     @Test
     public void registrationNegativeWrongEmail() {
@@ -51,9 +73,12 @@ public class RegistrationTests extends TestBase {
                 .withEmail("nefr"+i+"gmail.com")
                 .withPassword("Rita12300$");
         app.getUser().openRegestrationForm();
-        app.getUser().fillRegistrationForm(user);
+        app.getUser().fillRegistrationFormWith(user);
         app.getUser().submitLogin();
-        Assert.assertTrue(app.getUser().isLoggedError());
+        flagNeedOpenMainPage=true;
+        logger.info("flagNeedOpenMainPage = " + flagNeedOpenMainPage);
+        Assert.assertTrue(app.getUser().isLoggedErrorWrongEmail());
+       // app.getUser().buttonLogo();
 
 
 
@@ -62,15 +87,19 @@ public class RegistrationTests extends TestBase {
     @Test
     public void registrationNegativeEmptyPassword() {
         int i = (int) (System.currentTimeMillis() / 1000) % 3600;
-        User user = new User()
-                .withName("Sally")
-                .withLastName("Rotten")
-                .withEmail("nefr"+i+"@gmail.com")
-                .withPassword("");
+        UserLombok userLombok =  UserLombok.builder()
+                .name("Sally")
+                .lastName("Rotten")
+                .email("nefr"+i+"@gmail.com")
+                .password("")
+                .build();
         app.getUser().openRegestrationForm();
-        app.getUser().fillRegistrationForm(user);
+        app.getUser().fillRegistrationFormLombok(userLombok);
         app.getUser().submitLogin();
-        Assert.assertTrue(app.getUser().isLoggedError());
+        flagNeedOpenMainPage=true;
+        logger.info("flagNeedOpenMainPage = " + flagNeedOpenMainPage);
+        Assert.assertTrue(app.getUser().isLoggedErrorWrongPassword());
+       // app.getUser().buttonLogo();
 
     }
 
@@ -79,7 +108,7 @@ public class RegistrationTests extends TestBase {
     @AfterMethod
     public void postcondition(){
 
-        app.getUser().buttonOk();
+      //  app.getUser().buttonOk();
 
     }
 
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,9 +1,19 @@
 import manager.AplicationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.lang.reflect.Method;
+
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
   static   AplicationManager app = new AplicationManager();
+
+    boolean flagNeedLogout = false;
+    boolean flagNeedOpenMainPage = false;
 
 
     @BeforeSuite
@@ -19,6 +29,14 @@ public class TestBase {
        app.tearDown();
 
     }
+  public void startLogger(Method method) {
+    logger.info(method.getName() + "  is started ");
+  }
+  @AfterMethod
+  public void stopLogger() {
+    logger.info("================================================================================================================");
+  }
+
 
 
 }
